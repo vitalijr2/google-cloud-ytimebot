a6c50b6a474a3703b39b6e2833467eff0d80f9a9
Vitalij Berdinskih <vitalij_r2@outlook.com>
2024-03-25 19:57:32 +0200

    Initial import

---
+++ src/main/java/io/vitalijr2/ytimebot/YTimeBot.java
@@ -0,0 +1,14 @@
+package io.vitalijr2.ytimebot;
+
+import com.google.cloud.functions.HttpFunction;
+import com.google.cloud.functions.HttpRequest;
+import com.google.cloud.functions.HttpResponse;
+
+public class YTimeBot implements HttpFunction {
+
+  @Override
+  public void service(HttpRequest httpRequest, HttpResponse httpResponse) {
+
+  }
+
+}
